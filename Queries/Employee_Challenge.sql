70bb15070b970d07102074d45a7d7f725764a75c
Sara Obermeier <saramcel@gmail.com>
2022-08-15 18:57:52 -0500

    fixed csv and code for mentorship_eligibility

--- Queries/Employee_Challenge.sql
+++ Queries/Employee_Challenge.sql
@@ -37,6 +37,7 @@ GROUP BY title
 ORDER BY count DESC;
 
 -- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+DROP TABLE mentorship_eligibility;
 
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
     e.first_name,
@@ -44,13 +45,13 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
     e.birth_date,
     de.from_date,
     de.to_date,
-	t.title
+	ti.title
 	INTO mentorship_eligibility
 FROM employees as e
 INNER JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
-WHERE (t.to_date = '9999-01-01')
-     AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
