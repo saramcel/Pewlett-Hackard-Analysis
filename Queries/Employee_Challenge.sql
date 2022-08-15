414bd42fd70fdc5b8d81e65347a83b812a2b8fc2
Sara Obermeier <saramcel@gmail.com>
2022-08-15 18:20:28 -0500

    add new challenge, updated module files

---
+++ Queries/Employee_Challenge.sql
@@ -0,0 +1,56 @@
+-- Challenge for Module 7
+
+-- DELIVERABLE 1: The Number of Retiring Employees by Title 
+
+DROP TABLE ret_titles;
+--Join tables to get employee titles
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+	INTO ret_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Distinct with Order By to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, 
+first_name, 
+last_name, 
+title, 
+from_date,
+to_date
+INTO unique_titles
+FROM ret_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- The count of people who will retire counted by their title
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY count DESC;
+
+-- DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+	t.title
+	INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (t.to_date = '9999-01-01')
+     AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
--- Queries/schemas.sql
+++ Queries/schemas.sql
@@ -59,3 +59,35 @@ CREATE TABLE "titles" (
 );
 
 SELECT * FROM departments;
+
+--- adjustments when importing tables
+
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+DROP TABLE titles;
+
+CREATE TABLE titles (
+    emp_no int   NOT NULL,
+    title varchar   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title,from_date)
+);
+
+DROP TABLE dept_manager;
+
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
